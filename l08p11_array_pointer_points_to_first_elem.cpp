1aa3e9fb1694cf8a7126a1c56904a24b7a46e21f
andrespgd <42646798+andrespgd@users.noreply.github.com>
2021-09-06 09:20:08 -0500

    Create l08p11_array_pointer_points_to_first_elem.cpp

---
+++ l08p11_array_pointer_points_to_first_elem.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	int   arrayFive[5];
+
+	int* pArrayFive = arrayFive;
+
+	cout << "pArrayFive    = " << pArrayFive << endl;
+
+    cout << "&arrayFive[0] = "  << &arrayFive[0] << endl;
+
+	return 0;
+}
