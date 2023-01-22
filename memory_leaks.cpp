3a9eb163db32cb3f5b559743a384081eb8c477ea
andrespgd <42646798+andrespgd@users.noreply.github.com>
2023-01-22 10:26:44 -0600

    Create memory_leaks.cpp

---
+++ memory_leaks.cpp
@@ -0,0 +1,26 @@
+/*
+This code creates an infinite loop that continuously 
+allocates memory for an array of integers, 
+but never frees the memory by calling delete[] ptr. 
+
+This results in a memory leak, 
+as the program will eventually run out of memory and crash. 
+
+If you watch the memory usage it will peak 
+using almost all available memory,
+and then the process will be "killed" automatically.
+
+To fix this, the memory should be freed using 
+delete[] ptr before the end of the loop.
+*/
+
+#include <iostream>
+
+int main() {
+    while (true) {
+        // Allocate memory dynamically, but never free it
+        int* ptr = new int[1000000];
+    }
+
+    return 0;
+}
