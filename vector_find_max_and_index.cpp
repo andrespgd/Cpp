0418a8014f529cbcf477652589efdcc435161fc9
andrespgd <42646798+andrespgd@users.noreply.github.com>
2024-03-22 13:58:24 -0500

    Update vector_find_max_and_index.cpp

--- vector_find_max_and_index.cpp
+++ vector_find_max_and_index.cpp
@@ -2,20 +2,26 @@
 #include <iostream>
 #include <vector>
 
-using namespace std;
-
 int main() {
-  vector<int> v = {1, 2, 3, -3, -5};
+  std::vector<int> v = {1, 2, 3, 4, 5};
 
   // Find the iterator to the maximum element.
-  vector<int>::iterator it = max_element(v.begin(), v.end());
+  std::vector<int>::iterator it = std::max_element(v.begin(), v.end());
 
   // Get the value of the maximum element.
   int max_value = *it;
+  int max_value_position =  std::distance(v.begin(), it);
 
   // Print the argument and value of the maximum element.
-  cout << "max element at position: " << distance(v.begin(), it) << endl;
-  cout << "max element value: " << max_value << endl;
+  std::cout << "max element pos: " << max_value_position << std::endl;
+  std::cout << "max element val: "    << max_value          << std::endl;
+
+  // Print the vector to the screen
+  std::cout << "Vector is: " << std::endl;
+  for (int i = 0; i < v.size(); i++) {
+    std::cout << v[i] << " ";
+  }
+  std::cout << std::endl;
 
   return 0;
 }
